ffa0c47a0a7c6be02863bc8bd65b34a0f890a91e
Marek Gulbicki <mgulbicki73@gmail.com>
2020-07-14 11:42:29 +0200

    add content, styles and js functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,28 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const textarea = document.querySelector(".textarea--js");
+const load = document.querySelector(".load--js");
+const save = document.querySelector(".save--js");
+
+const currentValue = localStorage.getItem("entry");
+
+if (currentValue) {
+  document.querySelector(".info--js").innerHTML = "ℹ️";
+}
+
+save.addEventListener("click", (e) => {
+  e.preventDefault();
+  localStorage.setItem("entry", textarea.value);
+
+  if (textarea.value) {
+    document.querySelector(".info--js").innerHTML = "ℹ️";
+  } else {
+    document.querySelector(".info--js").innerHTML = "";
+  }
+});
+
+load.addEventListener("click", (e) => {
+  e.preventDefault();
+  textarea.value = localStorage.getItem("entry");
+});
